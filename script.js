2f029c26bfde6862b551f889c689cf804f2bba9b
luzneidy957 <luzneidyisturiz97@gmail.com>
2026-04-11 09:24:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
 // version 1.0.0
-
+let username = "";
 const readline = require('readline');
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-function getUserInput(question) {
-    return new Promise((resolve) => {
-        rl.question(question + " ", (answer) => {
-            resolve(answer);
-        });
-    });
-}
-
-// ------------------- VARIABLES INICIALES (Rúbrica) ----------------------
-let username = "";
-let palabraSecreta = "";
-let intentosRestantes = 3;
-let letrasAdivinadas = [];
-let bonoPuntos = 50;
-
-const listaPalabras = [
+let cedula = "33068401";
+let arregloAhorcado = [
     "abrazaderas", "abstracto", "abundancia", "acelerar", "acentuado", 
   "aceptacion", "acercamiento", "acertijo", "aclamacion", "aclaracion",
   "acompañante", "acondicionar", "aconsejar", "acostumbrar", "actualidad",
@@ -46,78 +30,93 @@ const listaPalabras = [
   "comestible", "comunicacion", "concentrar", "concluyente", "condicion",
   "conferencia", "confianza", "confirmar", "congelador", "conocimiento"
 ];
-
-// ------------------- FUNCIONES (Rúbrica) ----------------------
-
-function iniciarPartida(nombreJugador) {
-    console.log("¡Bienvenido/a " + nombreJugador + "! Tienes " + intentosRestantes + " intentos iniciales.");
+let palabraSecreta;
+let opcion = "";
+let palabrasUsadas = [];
+let vidas = 3;
+let victorias = 0;
+let derrotas = 0;
+let cantidadLetras;
+function getUserInput(question) {
+    return new Promise((resolve) => {
+        rl.question(question + " ", (answer) => {
+            resolve(answer);
+        });
+    });
 }
-
-function verificarLetra(letraIngresada, palabraOriginal) {
-    if (palabraOriginal.includes(letraIngresada)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function calcularPuntaje(intentosFinales, bono) {
-    return intentosFinales * bono;
-}
-
-// ------------------- LÓGICA PRINCIPAL ----------------------
-
-async function startGame() {
-    // 1. Pedir nombre (El await ahora está DENTRO de la función async)
+startGame();
+async function startGame(){
     username = await getUserInput("ingresa tu nombre");
-    iniciarPartida(username);
-
-    // 2. Configurar palabra
-    palabraSecreta = listaPalabras[Math.floor(Math.random() * listaPalabras.length)];
-    
-    for (let i = 0; i < palabraSecreta.length; i = i + 1) {
-        letrasAdivinadas.push("_");
-    }
-
-    // 3. Bucle de juego
-    while (intentosRestantes > 0 && letrasAdivinadas.includes("_")) {
-        console.log("\nPalabra: " + letrasAdivinadas.join(" "));
-        console.log("VIDAS: " + intentosRestantes);
-
-        let letra = await getUserInput("inserta una letra:");
-        let letraMinuscula = letra.toLowerCase();
-
-        if (letraMinuscula.length === 1) {
-            if (verificarLetra(letraMinuscula, palabraSecreta)) {
-                console.log("¡Correcto!");
-                for (let j = 0; j < palabraSecreta.length; j++) {
-                    if (palabraSecreta[j] === letraMinuscula) {
-                        letrasAdivinadas[j] = letraMinuscula;
+    do {
+        console.log("Bienvenido al juego del ahorcado", username);
+        console.log("Para empezar a jugar por favor coloque el numero 1");
+        console.log("para salir de programa coloque la cedula del autor:", cedula);
+        console.log("Buena suerte");
+        opcion = await getUserInput("Seleccione una opcion");
+        if (opcion === cedula) break;
+        switch (opcion) {
+            case "1":
+                if (palabrasUsadas.length === arregloAhorcado.length) {
+                    console.log("Ya se han usado todas las palabras, por favor cierre el juego... Gracias por jugar");
+                    opcion = cedula;
+                    break;
+                } else {
+                    do {
+                        palabraSecreta = arregloAhorcado[Math.floor(Math.random() * arregloAhorcado.length)].toLowerCase();
+                    } while (palabrasUsadas.includes(palabraSecreta));
+                    palabrasUsadas.push(palabraSecreta);
+                    cantidadLetras = palabraSecreta.length;
+                    vidas = 3;
+                    let letrasUsadas = [];
+                    let progreso = Array(cantidadLetras).fill("_");
+                    while (vidas > 0 && progreso.includes("_")) {
+                        console.log("\n=====================================");
+                        console.log("Palabra: " + progreso.join(" ") + " (" + cantidadLetras + " letras)");
+                        console.log("");
+                        console.log("Vidas: " + vidas + " | Letras usadas: " + letrasUsadas.join(","));
+                        console.log("Victorias: " + victorias + " | Derrotas: " + derrotas);
+                        let entrada = await getUserInput("Ingresa una letra:");
+                        if (entrada === cedula) {
+                            opcion = cedula;
+                            break;
+                        }
+                        let letra = entrada.toLowerCase();
+                        if (letra.length !== 1 || !/[a-z]/.test(letra) || letra === "ñ") {
+                            console.log("ERROR: Ingresa solo una letra válida (a-z, sin ñ).");
+                            continue;
+                        }
+                        if (letrasUsadas.includes(letra)) {
+                            console.log("Ya utilizaste la letra: " + letra);
+                            continue;
+                        }
+                        letrasUsadas.push(letra);
+                        if (palabraSecreta.includes(letra)) {
+                            console.log("¡Letra correcta!");
+                            for (let i = 0; i < palabraSecreta.length; i++) {
+                                if (palabraSecreta[i] === letra) progreso[i] = letra;
+                            }
+                        } else {
+                            console.log("Letra incorrecta.");
+                            vidas--;
+                        }
+                        await getUserInput("Presiona Enter para continuar...");
                     }
+                    if (opcion === cedula) break;
+                    if (!progreso.includes("_")) {
+                        console.log("\n¡Felicidades " + username + "! Ganaste. La palabra era: " + palabraSecreta);
+                        victorias++;
+                    } else {
+                        console.log("\nPerdiste todas tus vidas. La palabra era: " + palabraSecreta);
+                        derrotas++;
+                    }
+                    await getUserInput("Presiona Enter para volver al menú...");
+                    break;
                 }
-            } else {
-                intentosRestantes = intentosRestantes - 1;
-                console.log("Letra incorrecta.");
-                
-                // Validación especial (Rúbrica)
-                if (intentosRestantes === 1) {
-                    console.log("¡Cuidado! Te queda un solo intento.");
-                }
-            }
+            default:
+                if (opcion !== cedula) console.log("Opción no válida.");
+                break;
         }
-    }
-
-    // 4. Fin del juego
-    if (!letrasAdivinadas.includes("_")) {
-        let puntos = calcularPuntaje(intentosRestantes, bonoPuntos);
-        console.log("\n¡GANASTE " + username + "! Tu puntaje es: " + puntos);
-    } else {
-        console.log("\nPerdiste. La palabra era: " + palabraSecreta);
-    }
-
+    } while (opcion !== cedula);
     return rl.close();
 }
-
-// Invocación única
-startGame();
-
+console.log("Gracias por jugar, espero te haya gustado......");
